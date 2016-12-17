e663f08d4edec277e494fb5aea9bffa6c0830172
ninxxxxx <ninxxxxxx@gmail.com>
2016-12-17 22:36:58 +0700

    for gay porm to create sortByDate pipe in home

--- src/components/add-event/add-event.ts
+++ src/components/add-event/add-event.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-
+import { ViewController} from 'ionic-angular';
+import {Validators, FormControl} from '@angular/forms';
 /*
   Generated class for the AddEvent component.
 
   See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
   for more info on Angular 2 Components.
-*/
-@Component({
-  selector: 'add-event',
-  templateUrl: 'add-event.html'
-})
-export class AddEventComponent {
+    */
+  @Component({
+    selector: 'add-event',
+    templateUrl: 'add-event.html'
+  })
+  export class AddEventComponent {
+    titleControl: any;
+    event: any;
+    constructor(public viewCtrl: ViewController) {
+      this.titleControl = new FormControl('', Validators.compose([Validators.nullValidator, Validators.required]));
+      this.event = {
+        title: "",
+        startDate: new Date(),
+        endDate: new Date(),
+        isStricted: false,
+        strictedParticipants:[],
+        participants:[]
+      }
+    }
 
-  text: string;
 
-  constructor() {
-    console.log('Hello AddEvent Component');
-    this.text = 'Hello World';
+
+    dismiss(){
+      this.viewCtrl.dismiss();
+    }
+
+    saveEvent(event){
+      console.log(event);
+    }
   }
-
-}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+
+import { AddEventComponent } from '../../components/add-event/add-event';
 
 /*
   Generated class for the Home page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
+  */
+  @Component({
+  	selector: 'page-home',
+  	templateUrl: 'home.html'
+  })
+  export class HomePage {
+  	events: [any];
+  	constructor(public navCtrl: NavController, public modalCtrl: ModalController)
+  	{
+  		this.events = [
+      {
+        gg: 1,
+        title: "Mom Day", 
+        startDate: "12/08/2559",
+        endDate: "13/08/2559",
+        stricted: false,
+        strictedParticipants: [],
+        participants: [
+        "5610110655",
+        "5610110654",
+        "5610110651",
+        ],
 
-  constructor(public navCtrl: NavController) {}
+      },
+      {
+        gg: 2,
+        title: "Dad Day", 
+        startDate: "05/12/2559",
+        endDate: "06/12/2559",
+        stricted: true,
+        strictedParticipants: [
+        "5610110655",
+        "5610110654",
+        ],
+        participants: [
+        "5610110655",
+        "5610110654",
+        "5610110651",
+        "5610110651",
+        "5710343344"
+        ],
 
-  ionViewDidLoad() {
-    console.log('Hello HomePage Page');
+      },
+      ];
+    }
+
+    ionViewDidLoad() {
+      console.log('Hello HomePage Page');
+    }
+
+    showAddEventModal(){
+      let modal = this.modalCtrl.create(AddEventComponent);
+      modal.present();
+    }
   }
-
-}
